58c26e58dca8fc09398012854b4a8c1630416c84
Felipe Morales <felipe.morales.querol@gmail.com>
2022-04-03 10:25:17 -0300

    Agregado de TODOs

--- ejemplos/01.cuenta-bancaria/cuentaBancaria.py
+++ ejemplos/01.cuenta-bancaria/cuentaBancaria.py
@@ -1,15 +1,23 @@
 class CuentaBancaria():
+    ''' Clase que nos permite la gestión de una Cuenta Bancaria genérica
+    '''
     
     def __init__(self, saldo_inicial, nombre, apellido):
+        # TODO: Guardar en un lista todo los movimientos de saldo con su fecha
+        # TODO: Agregar un atributo de moneda
+        # TODO: Ver la forma de soportar cajas de ahorro y/o cuentas corrientes
         self.saldo = saldo_inicial
         self.nombre = nombre
         self.apellido = apellido
 
     def depositar(self, monto):
+        '''Método que nos permite realizar un depósito bancario'''
         self.saldo = self.saldo + monto
 
 
     def extraer(self, monto):
+        # TODO: Agregar documentación inline (https://www.youtube.com/watch?v=SJqANWdRG4I)
+        # TODO: Considerar el caso de saldo negativo
         self.saldo = self.saldo - monto
         
 
@@ -19,12 +27,4 @@ class CuentaBancaria():
     def datos_saldo(self):
         return self.saldo
 
-# felipe_morales = CuentaBancaria('Felipe', 'Morales')
-
-# # Invocación de métodos
-# print(felipe_morales.datos_titular())
-
-
-# # Operación de deposito
-# felipe_morales.depositar(100000)
-# print(felipe_morales.datos_saldo())        
+     
