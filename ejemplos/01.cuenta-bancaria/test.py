afae5253ea32318413892d32e8089bf960d6f6e4
Felipe Morales <felipe.morales.querol@gmail.com>
2022-04-12 23:50:15 -0300

    Fix for review

--- ejemplos/01.cuenta-bancaria/test.py
+++ ejemplos/01.cuenta-bancaria/test.py
@@ -1,22 +1,24 @@
 from cuentaBancaria import CuentaBancaria
 
 # Creación de objeto
-felipe_morales = CuentaBancaria(0, 'Felipe', 'Morales')
+x = CuentaBancaria(10, 'Felipe', 'Morales')
 
 # Invocación de métodos
-print(felipe_morales.datos_titular())
+print(x.datos_titular())
 
 
 # Operación de deposito
-felipe_morales.depositar(100000)
-print(felipe_morales.datos_saldo())
+x.depositar(100000)
+print(x.datos_saldo())
+
 
 
 # Operación de extracción
-felipe_morales.extraer(900000)
-print(felipe_morales.datos_saldo())
+x.extraer(900000)
+print(x.datos_saldo())
 
 #felipe_morales.reset_saldo()
-print(dir(felipe_morales))
-print(felipe_morales.datos_saldo())
+#print(dir(x))
 
+# Imprimo el listado de movimientos de la cuenta
+print(x.datos_movimientos())
